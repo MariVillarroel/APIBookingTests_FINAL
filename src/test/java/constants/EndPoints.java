4f5d037d82d686edebd6cee7dff01aef1eb073e8
MariVillarroel <mvillarroel.deheza@gmail.com>
2025-09-11 17:54:17 -0400

    Agregamos EndPoints

---
+++ src/test/java/constants/EndPoints.java
@@ -0,0 +1,8 @@
+package constants;
+
+public class EndPoints {
+    public static final String BASE_URL = "https://restful-booker.herokuapp.com";
+    public static final String AUTH = "/auth";
+    public static final String BOOKING = "/booking";
+    public static final String BOOKING_BY_ID = "/booking/{id}";
+}
