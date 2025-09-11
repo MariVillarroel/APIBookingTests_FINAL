3d84d77537cead8fa345d730a0b8bdddfff18d80
MariVillarroel <mvillarroel.deheza@gmail.com>
2025-09-11 18:08:22 -0400

    Agregamos modelos

---
+++ src/test/java/entities/Auth.java
@@ -0,0 +1,11 @@
+package entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Auth {
+    private String username;
+    private String password;
+}
---
+++ src/test/java/entities/BookingDates.java
@@ -0,0 +1,11 @@
+package entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BookingDates {
+    private String checkin;
+    private String checkout;
+}
